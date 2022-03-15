c6ca87e2cadcf2513f33d3ef47aaeb2ac151a46e
owrx887 <owrx887@gmail.com>
2022-03-15 23:06:45 +0900

    update

---
+++ src/algorithm/bfsdfs.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class bfsdfs {
+	static int T,N,M,K;
+	static int[][] map;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static int count;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		T = sc.nextInt();
+		
+		for (int t = 0; t < T; t++) {
+			M = sc.nextInt();
+			N = sc.nextInt();
+			K = sc.nextInt();
+			
+			map = new int[N][M];
+			count = 0;
+			
+			for (int i = 0; i < K; i++) {
+				
+			}
+		}
+	}
+
+}
