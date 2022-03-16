fd59083c69bb2771ca4ca3624f67c74f5f074274
owrx887 <owrx887@gmail.com>
2022-03-17 00:31:19 +0900

    update

--- src/algorithm/bfsdfs.java
+++ src/algorithm/bfsdfs.java
@@ -3,11 +3,11 @@ package algorithm;
 import java.util.Scanner;
 
 public class bfsdfs {
-	static int T,N,M,K;
+	static int T,N,M,K; //테스트개수, 가로길이, 세로길이, 위치의 개수
 	static int[][] map;
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
-	static int count;
+	static int count; //최소 개수
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -30,7 +30,7 @@ public class bfsdfs {
 			
 			for (int y = 0; y < N; y++) {
 				for (int x = 0; x < M; x++) {
-					if(map[y][x] == 1) {
+					if(map[y][x] == 1) { //배추 발견
 						dfs(y, x);
 						count++;
 					}
@@ -41,13 +41,13 @@ public class bfsdfs {
 	}
 	
 	public static void dfs(int y, int x) {
-		map[y][x] = 2;
+		map[y][x] = 2; //확인한 배추는 2로 수정
 		for (int i = 0; i < 4; i++) {
 			int targetY = y + dy[i];
 			int targetX = x + dx[i];
 			
 			if(targetY >= 0 && targetY < N && targetX >= 0 && targetX < M) {
-				if(map[targetY][targetX] == 1) {
+				if(map[targetY][targetX] == 1) { //주변에 배추 있으면 다시 반복
 					dfs(targetY, targetX);
 				}
 			}
--- src/algorithm/cardgame.java
+++ src/algorithm/cardgame.java
@@ -6,12 +6,12 @@ public class cardgame {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] color = new int[4];
-		int[] num = new int[10];
+		int[] color = new int[4]; //4가지 생상
+		int[] num = new int[10]; //9가지 숫자
 		
 		for (int i=0; i<5; i++) {
-			String temp = sc.next(); //다음행으로
-			char tempColor = temp.charAt(0); //int에서char로형변환
+			String temp = sc.next(); //String 받고 다음행으로
+			char tempColor = temp.charAt(0); //int에서 char로 형변환
 			switch(tempColor) {
 			case 'R':
 				color[0]++;
@@ -26,15 +26,15 @@ public class cardgame {
 				color[3]++;
 				break;
 		}
-			int tempNum = sc.nextInt(); //다음행으로
-			num[tempNum]++;
+			int tempNum = sc.nextInt(); //int 받고 다음행으로
+			num[tempNum]++; //중복 횟수
 	}
 		
 		boolean isFlush = checkFlush(color);
 		int straight = checkStraight(num);
 		int fourCard = checkFourCard(num);
 		int triple = checkTriple(num);
-		int pair = checkPair(num, 1);
+		int pair = checkPair(num, 1); //여러개 가능해서 시작점 만듬
 		int max = getMax(num);
 		
 		if(isFlush == true && straight > 0) {
@@ -58,7 +58,7 @@ public class cardgame {
 		}
 	}
 		
-		
+		//모두 같은 색
 		public static boolean checkFlush(int[] color) {
 			for (int i = 0; i < color.length; i++) {
 				if(color[i] == 5) {
@@ -68,6 +68,7 @@ public class cardgame {
 			return false;
 		}
 		
+		//숫자가 연속적
 		public static int checkStraight(int[] num) {
 			int count = 0;
 			for (int i = 1; i < num.length; i++) {
@@ -76,14 +77,14 @@ public class cardgame {
 					if(count == 5) {
 						return i;
 					}
-					else {
-						count = 0;
-					}
+				}else {
+					count = 0;
 				}
 			}
 			return 0;
 		}
 		
+		//4장의 숫자가 같음
 		public static int checkFourCard(int[] num) {
 			for (int i =1; i < num.length; i++) {
 				if(num[i] == 4) {
@@ -93,6 +94,7 @@ public class cardgame {
 			return 0;
 		}
 		
+		//3장의 숫자가 같음
 		public static int checkTriple(int[] num) {
 			for (int i =1; i<num.length; i++) {
 				if(num[i] == 3) {
@@ -102,6 +104,7 @@ public class cardgame {
 			return 0;
 		}
 		
+		//2장의 숫자가 같음 (또 다른 2장의 숫자가 같을 수 있음)
 		public static int checkPair(int[] num, int start) {
 			for (int i = start; i<num.length; i++) {
 				if (num[i] == 2) {
@@ -111,6 +114,7 @@ public class cardgame {
 			return 0;
 		}
 		
+		//가장 큰 숫자
 		public static int getMax(int[] num) {
 			for (int i = num.length-1; i>0; i--) {
 				if(num[i] > 0) {
